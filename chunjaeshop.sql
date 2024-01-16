e88a160bd0b5531fe7dbe0e2c0274f4f837e4dd9
ww <wwnoov@gmail.com>
2024-01-16 13:33:11 +0900

    docs: sql문 수정

--- chunjaeshop.sql
+++ chunjaeshop.sql
@@ -148,12 +148,9 @@ CREATE TABLE cart_detail (
 
 CREATE TABLE comment (
                          comment_idx  INT PRIMARY KEY AUTO_INCREMENT,
-                         review_idx INT, #외래키
-                                      customer_idx INT, # 외래키
-                             product_idx INT, # 외래키
-                             commentContents varchar(200),
-                         commentCreatedTime DATETIME DEFAULT CURRENT_TIMESTAMP,
-                         FOREIGN KEY (review_idx) REFERENCES product_review (review_idx) ON UPDATE CASCADE ON DELETE SET NULL,
-                         FOREIGN KEY (customer_idx) REFERENCES customer (customer_idx) ON UPDATE CASCADE ON DELETE SET NULL,
+                         product_idx INT, # 외래키
+                         comment_writer varchar(50),
+                         comment_contents varchar(200),
+                         comment_createdTime DATETIME DEFAULT CURRENT_TIMESTAMP,
                          FOREIGN KEY (product_idx) REFERENCES product (product_idx) ON UPDATE CASCADE ON DELETE SET NULL
 );
