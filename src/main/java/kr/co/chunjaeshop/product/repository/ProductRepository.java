223f7ab5891ef20fbce95179cc1c040053aabb73
Gyeongrak Choi <peter6617@gmail.com>
2024-01-17 19:38:06 +0900

    style : 불필요한 주석 삭제

--- src/main/java/kr/co/chunjaeshop/product/repository/ProductRepository.java
+++ src/main/java/kr/co/chunjaeshop/product/repository/ProductRepository.java
@@ -23,7 +23,6 @@ public interface ProductRepository {
     int productImgUpdate(ProductMainImgUpdateDTO productMainImgUpdateDTO);
     ProductDetailImgUpdateDTO findDetailImg(Integer sellerIdx, Integer productIdx);
     int productDetailImgUpdate(ProductDetailImgUpdateDTO productDetailImgUpdateDTO);
-    //List<ProductDTO> getList(Integer categoryIdx);
     List<ProductDTO> productListPagingWithSearch(Map<String, Object> pagingParams);
     int productListCount(Integer categoryIdx);
     int searchProductListCount(Integer categoryIdx, String searchField, String searchWord);
